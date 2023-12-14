bc6db31282d553e8ec7d6c08ccc2cb8c5d54ef9e
dinoddg <avharshith2005@gmail.com>
2023-12-14 16:58:06 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
-function makingGrid() {
-  let grid = document.getElementById("grid");
+function randomPath() {
+  let paths = [
+    ["15", "25", "24", "34", "33", "43", "42"]
+    // ["23", "33", "34", "35", "25", "15"],
+  ];
 
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
-      let templateCell = `<div class="cell-block" id="${i}${j}" name="box"></div>`;
-      grid.innerHTML += templateCell;
-    }
-  }
+  let randomNum = Math.floor(Math.random() * paths.length);
 
-  let elements = document.getElementsByName("box");
-  let num = 1;
-  for (let i = 0; i < elements.length; i++) {
-    if (elements[i].id == "00") {
-      elements[i].style.backgroundColor = "transparent";
-      elements[i].style.border = "0px";
-    } else if (
-      elements[i].id == "01" ||
-      elements[i].id == "02" ||
-      elements[i].id == "03" ||
-      elements[i].id == "04" ||
-      elements[i].id == "05"
-    ) {
-      elements[i].style.backgroundColor = "transparent";
-      elements[i].style.border = "0px";
-      elements[i].innerHTML += `<h1 class="coord">${i}</h1>`;
-    } else if (
-      elements[i].id == "10" ||
-      elements[i].id == "20" ||
-      elements[i].id == "30" ||
-      elements[i].id == "40" ||
-      elements[i].id == "50"
-    ) {
-      elements[i].style.backgroundColor = "transparent";
-      elements[i].style.border = "0px";
-      elements[i].innerHTML += `<h1 class="coord">${num}</h1>`;
-      num += 1;
-    }
-  }
+  return paths[randomNum]
 }
 
-makingGrid()
+let selectedPath = randomPath()
+let startingPoint = document.getElementById(selectedPath[0])
+let elements = document.querySelectorAll(".cell-block")
+startingPoint.innerHTML += `<img src="./assets/minotaur (1).png" id="playerIcon">`
+console.log(elements)
 
-let start = document.getElementById("11")
+elements.forEach((box) => {
+    box.onclick = () => {
+        console.log(box)
+    }
+})
